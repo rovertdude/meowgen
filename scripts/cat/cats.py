74ad5c9abfdc1a6d86d268fa68e071f3a54fcdc9
Espeon <tsannoyingorange@gmail.com>
2024-08-22 14:58:24 -0500

    allow related cats to be mated if first cousin mating is enabled

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -2418,7 +2418,8 @@ class Cat:
 
         # Inheritance check
         if self.is_related(other_cat, first_cousin_mates):
-            return False
+            if first_cousin_mates):
+                return False
 
         # check exiled, outside, and dead cats
         if (self.dead != other_cat.dead) or self.outside or other_cat.outside:
